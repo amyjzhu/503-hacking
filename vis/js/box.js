55a970c9becfcd8b069777146c5db3295d0fe86a
Amy Zhu <ajyzhu@gmail.com>
2021-02-16 16:48:07 -0800

    Add infrastructure for methods, but strangely the placement is slightly off

--- vis/js/box.js
+++ vis/js/box.js
@@ -20,16 +20,21 @@ class StructureVis {
         this.boxHeight = 100;
         this.smallBoxWidth = this.boxWidth / 3;
         this.smallBoxHeight = this.boxHeight / 3;
-        // this.colours = ["red", "blue", "yellow", "green"]
+
+        this.smallestBoxHeight = this.smallBoxHeight / 2;
+        this.smallestBoxWidth = this.smallBoxWidth / 2;
+
+        // this.colours = ["red", "blue", "yellow", "green"]\
+        // TODO we should make the colours get lighter or darker by level
         this.colours = d3.schemePastel1;
 
         this.zoomBarHeight = 300;
-        this.minZoom = 0.5;
-        this.maxZoom = 10;
+        this.minZoom = 0.25;
+        this.maxZoom = 20;
         this.zoomLevel = 1;
 
         // this.transitionPoints = [0.25];
-        this.transitionPoints = [0.15];
+        this.transitionPoints = [0.075, 0.5];
         this.initialized = 0;
 
         this.view = _config.view || "default"
@@ -39,17 +44,17 @@ class StructureVis {
 
     initVis() {
         let vis = this;
-        
+
         vis.center = {
             x: vis.width / 2,
             y: 1.25 * vis.height / 3
         };
 
         vis.colourScale = d3.scaleOrdinal()
-            .domain([1, 2, 3, 4]) 
+            .domain([1, 2, 3, 4])
             .range(vis.colours)
 
-        vis.boxData = vis.data.packages.concat(vis.data.classes);
+        vis.boxData = vis.data.packages.concat(vis.data.classes).concat(vis.data.methods);
 
         vis.visArea = vis.chart.append("g");
 
@@ -64,6 +69,13 @@ class StructureVis {
         vis.data.packages.map(p => p.id).forEach(id => {
             vis.classesByPackage[id] = vis.data.classes.filter(c => c.pkg == id);
         });
+
+        vis.methodsByClass = {};
+        vis.data.classes.map(c => c.id).forEach(id => {
+            vis.methodsByClass[id] = vis.data.methods.filter(m => m.cls == id);
+        });
+
+        console.log(vis.methodsByClass);
 
         function charge() {
             return -Math.pow(vis.boxHeight, 2.2) * vis.forceStrength;
@@ -84,22 +96,44 @@ class StructureVis {
             var mapping = {};
             mapping.id = pkg;
             mapping.simFn = () => {
-            var pkgInfo = vis.data.packages.find(p => p.id == pkg);
-            console.log(pkgInfo.x + vis.boxWidth/2);
+                var pkgInfo = vis.data.packages.find(p => p.id == pkg);
+                console.log(pkgInfo.x + vis.boxWidth / 2);
 
-            return d3.forceSimulation()
-            .velocityDecay(0.18)
-            .force('x', d3.forceX().strength(vis.forceStrength).x(pkgInfo.x + vis.boxWidth / 2))
-            .force('y', d3.forceY().strength(vis.forceStrength).y(pkgInfo.y + vis.boxHeight / 2))
-            .force('charge', d3.forceManyBody().strength(() => -Math.pow(vis.smallBoxHeight, 2) * vis.forceStrength))
-            .force('collision', d3.forceCollide().radius(d => Math.sqrt(Math.pow(vis.smallBoxWidth / 2, 2) + Math.pow(vis.smallBoxHeight / 2, 2))))
-            .force("center", d3.forceCenter(pkgInfo.x + vis.boxWidth/2, pkgInfo.y + vis.boxHeight / 2))
-            .on('tick', () => vis.classTicked(vis));
+                return d3.forceSimulation()
+                    .velocityDecay(0.18)
+                    .force('x', d3.forceX().strength(vis.forceStrength).x(pkgInfo.x + vis.boxWidth / 2))
+                    .force('y', d3.forceY().strength(vis.forceStrength).y(pkgInfo.y + vis.boxHeight / 2))
+                    .force('charge', d3.forceManyBody().strength(() => -Math.pow(vis.smallBoxHeight, 2) * vis.forceStrength))
+                    .force('collision', d3.forceCollide().radius(d => Math.sqrt(Math.pow(vis.smallBoxWidth / 2, 2) + Math.pow(vis.smallBoxHeight / 2, 2))))
+                    .force("center", d3.forceCenter(pkgInfo.x + vis.boxWidth / 2, pkgInfo.y + vis.boxHeight / 2))
+                    .on('tick', () => vis.classTicked(vis));
             }
             mapping.cls = vis.classesByPackage[pkg];
             return mapping;
         });
-        
+
+        vis.perClassSimulations = Object.keys(vis.methodsByClass).map(cls => {
+            // TODO maybe remove if there is no sim
+            var mapping = {};
+            mapping.id = cls;
+            mapping.simFn = () => {
+                var clsInfo = vis.data.classes.find(c => c.id == cls);
+                console.log(clsInfo.x + vis.boxWidth / 2);
+
+                return d3.forceSimulation()
+                    .velocityDecay(0.18)
+                    .force('x', d3.forceX().strength(vis.forceStrength).x(clsInfo.x + vis.smallBoxWidth / 2))
+                    .force('y', d3.forceY().strength(vis.forceStrength).y(clsInfo.y + vis.smallBoxHeight / 2))
+                    .force('charge', d3.forceManyBody().strength(() => -Math.pow(vis.smallestBoxHeight, 2.1) * vis.forceStrength))
+                    .force('collision', d3.forceCollide().radius(d => Math.sqrt(Math.pow(vis.smallestBoxWidth / 2, 2) + Math.pow(vis.smallestBoxHeight / 2, 2))))
+                    .force("center", d3.forceCenter(clsInfo.x + vis.smallBoxWidth / 2, clsInfo.y + vis.smallBoxWidth / 2))
+                    .on('tick', () => vis.methodTicked(vis));
+            }
+            mapping.cls = vis.methodsByClass[cls];
+            return mapping;
+        });
+
+        console.log(vis.perClassSimulations)
         console.log(vis.perPkgSimulations)
 
         vis.addZoomLevel();
@@ -117,8 +151,10 @@ class StructureVis {
 
         if (this.viewLevel == "package") {
             vis.linkData = vis.data.packageLinks;
-        } else {
+        } else if (this.viewLevel == "class") {
             vis.linkData = vis.data.classLinks;
+        } else {
+            vis.linkData = vis.data.methodLinks;
         }
 
         vis.render();
@@ -128,7 +164,7 @@ class StructureVis {
         let vis = this;
 
         var zoom = d3.zoom()
-        .scaleExtent([vis.minZoom, vis.maxZoom]);
+            .scaleExtent([vis.minZoom, vis.maxZoom]);
 
         vis.svg
             .call(zoom.on("zoom", function () {
@@ -153,6 +189,7 @@ class StructureVis {
             // colour coded by group
             .style("fill", d => vis.viewLevel == "package" ? vis.colourScale(d.group) : "none")
             .style("stroke", d => vis.viewLevel == "package" ? "none" : vis.colourScale(d.group))
+            .transition()
             .style("visibility", d => vis.view == "default" || d.views.includes(vis.view) ? "visible" : "hidden");
         console.log(boxes)
 
@@ -160,18 +197,39 @@ class StructureVis {
         var classGroupsSelect = vis.boxArea.selectAll(".classGroup").data(vis.boxData.filter(d => d.type == "package"), d => { return d.type + d.id });
         vis.classGroups = classGroupsSelect.enter().append("g").attr("class", "classGroup").merge(classGroupsSelect);
 
-        vis.perClassGroup = vis.classGroups.selectAll("g").data(pkg => { console.log(vis.boxData.filter(d => d.type == "class" && d.pkg == pkg.id)); return vis.boxData.filter(d => d.type == "class" && d.pkg == pkg.id)}, d => d.type + d.id );
+        vis.perClassGroup = vis.classGroups.selectAll("g").data(pkg => { console.log(vis.boxData.filter(d => d.type == "class" && d.pkg == pkg.id)); return vis.boxData.filter(d => d.type == "class" && d.pkg == pkg.id) }, d => d.type + d.id);
         vis.perClassGroup = vis.perClassGroup.enter().append("g").merge(vis.perClassGroup);
-       
+
         vis.classRects = vis.perClassGroup.selectAll("rect").data(d => [d]);
         vis.classRects = vis.classRects.enter()
-        .append("rect")
-        .attr("class", "class-box")
-        .merge(vis.classRects)
-        .attr("width", vis.smallBoxWidth)
-        .attr("height", vis.smallBoxHeight)
-        .style("fill", d => vis.viewLevel == "package" ? "none" : vis.colourScale(d.group))
-        .style("visibility", d => { console.log(d.views); return vis.view == "default" || d.views.includes(vis.view) ? "visible" : "hidden"});
+            .append("rect")
+            .attr("class", "class-box")
+            .merge(vis.classRects)
+            .attr("width", vis.smallBoxWidth)
+            .attr("height", vis.smallBoxHeight)
+            .style("fill", d => vis.viewLevel == "class" ? vis.colourScale(d.group) : "none")
+            .style("stroke", d => vis.viewLevel == "method" ? vis.colourScale(d.group) : "none")
+            .transition()
+            .style("visibility", d => { console.log(d.views); return vis.view == "default" || d.views.includes(vis.view) ? "visible" : "hidden" });
+
+        // methods
+        var methodGroupsSelect = vis.boxArea.selectAll(".methodGroup").data(vis.boxData.filter(d => d.type == "class"), d => { return d.type + d.id });
+        vis.methodGroups = methodGroupsSelect.enter().append("g").attr("class", "methodGroup").merge(methodGroupsSelect);
+
+        vis.perMethodGroup = vis.methodGroups.selectAll("g").data(cls => { return vis.boxData.filter(d => d.type == "method" && d.cls == cls.id) }, d => d.type + d.id);
+        vis.perMethodGroup = vis.perMethodGroup.enter().append("g").merge(vis.perMethodGroup);
+
+        vis.methRects = vis.perMethodGroup.selectAll("rect").data(d => [d]);
+        vis.methRects = vis.methRects.enter()
+            .append("rect")
+            .attr("class", "class-box")
+            .merge(vis.methRects)
+            .attr("width", vis.smallestBoxWidth)
+            .attr("height", vis.smallestBoxHeight)
+            .style("fill", d => vis.viewLevel == "method" ? vis.colourScale(d.group) : "none")
+            .transition()
+            .style("visibility", d => { console.log(d.views); return vis.view == "default" || d.views.includes(vis.view) ? "visible" : "hidden" });
+
 
 
         var texts = vis.boxGroups.selectAll("text").data(d => [d]);
@@ -204,13 +262,13 @@ class StructureVis {
         vis.links.exit().remove();
 
         if (vis.initialized == 0) {
-        vis.simulation.nodes(vis.boxData).restart()
-        // this lower line is optional
-        .force("link", d3.forceLink(vis.linkData).id(d => d.type + d.id));
+            vis.simulation.nodes(vis.boxData).restart()
+                // this lower line is optional
+                .force("link", d3.forceLink(vis.linkData).id(d => d.type + d.id));
 
-        vis.initialized = 1;
+            vis.initialized = 1;
         }
-        
+
     }
 
     ticked(vis) {
@@ -223,8 +281,15 @@ class StructureVis {
 
     classTicked(vis) {
         vis.perClassGroup
-        .attr("transform", d => `translate(${d.x}, ${d.y})`);
-        
+            .attr("transform", d => `translate(${d.x}, ${d.y})`);
+
+        vis.updateLinks();
+    }
+
+    methodTicked(vis) {
+        vis.perMethodGroup
+            .attr("transform", d => `translate(${d.x}, ${d.y})`);
+
         vis.updateLinks();
     }
 
@@ -233,23 +298,24 @@ class StructureVis {
 
         // having links go across different force graphs seems a bit troublesome...
         // so this is a hacky workaround for now
+        // we can also count the number of nodes... connected and fade them out if they're too far away
+        // 
         if (vis.viewLevel == "package") {
             vis.links
-            .attr("x1", d => d.source.x)
-            .attr("y1", d => d.source.y)
-            .attr("x2", d => d.target.x)
-            .attr("y2", d => d.target.y)
-            .style("visibility", d => vis.view == "default" || (d.source.views.includes(vis.view) && d.target.views.includes(vis.view)) ? "visible" : "hidden");
+                .attr("x1", d => d.source.x)
+                .attr("y1", d => d.source.y)
+                .attr("x2", d => d.target.x)
+                .attr("y2", d => d.target.y)
+                .style("visibility", d => vis.view == "default" || (d.source.views.includes(vis.view) && d.target.views.includes(vis.view)) ? "visible" : "hidden");
 
         } else {
-        vis.links
-            .attr("x1", d => vis.boxData.find(data => d.source == data.type + data.id).x)
-            .attr("y1", d => vis.boxData.find(data => d.source == data.type + data.id).y)
-            .attr("x2", d => vis.boxData.find(data => d.target == data.type + data.id).x)
-            .attr("y2", d => vis.boxData.find(data => d.target == data.type + data.id).y)
-            .style("visibility", d => vis.view == "default" || (vis.boxData.find(data => d.source == data.type + data.id).views.includes(vis.view) 
-            && vis.boxData.find(data => d.target == data.type + data.id).views.includes(vis.view)) ? "visible" : "hidden");
-
+            vis.links
+                .attr("x1", d => vis.boxData.find(data => d.source == data.type + data.id).x)
+                .attr("y1", d => vis.boxData.find(data => d.source == data.type + data.id).y)
+                .attr("x2", d => vis.boxData.find(data => d.target == data.type + data.id).x)
+                .attr("y2", d => vis.boxData.find(data => d.target == data.type + data.id).y)
+                .style("visibility", d => vis.view == "default" || (vis.boxData.find(data => d.source == data.type + data.id).views.includes(vis.view)
+                    && vis.boxData.find(data => d.target == data.type + data.id).views.includes(vis.view)) ? "visible" : "hidden");
         }
     }
 
@@ -261,33 +327,67 @@ class StructureVis {
 
         // TODO hacky, need to fix this impl later for extensibility
         var viewThreshold = vis.transitionPoints[0] * (vis.maxZoom - vis.minZoom);
-        
-        if (vis.zoomLevel >= viewThreshold && vis.viewLevel == "package") {
-                vis.simulation.stop();
-                vis.viewLevel = "class";
-            
-                vis.perPkgSimulations.forEach(sim => {
-                    if (sim.sim == undefined) {
+        var viewThresholdMethod = vis.transitionPoints[1] * (vis.maxZoom - vis.minZoom);
+
+        if (vis.zoomLevel >= viewThreshold && vis.zoomLevel <= viewThresholdMethod && vis.viewLevel != "class") {
+            vis.simulation.stop();
+            vis.viewLevel = "class";
+
+            vis.perPkgSimulations.forEach(sim => {
+                if (sim.sim == undefined) {
                     // now we need to create it, if it's the first time...
-                        sim.sim = sim.simFn();
-                        sim.sim.nodes(sim.cls).restart();
-                    }
-                })
+                    sim.sim = sim.simFn();
+                    sim.sim.nodes(sim.cls).restart();
+                }
+            })
 
-                vis.update();
-                vis.updateLinks();
-
-        } else if (vis.zoomLevel <= viewThreshold && vis.viewLevel == "class") {
-                vis.simulation.stop();
-
-                vis.perPkgSimulations.forEach(sim => {
+            vis.perClassSimulations.forEach(sim => {
+                if (sim.sim != undefined) {
                     sim.sim.stop();
-                })
+                }
+            })
 
-                vis.viewLevel = "package";
-                vis.update();
-                vis.updateLinks();
+            vis.update();
+            vis.updateLinks();
 
+        } else if (vis.zoomLevel <= viewThreshold && vis.viewLevel != "package") {
+            vis.simulation.stop();
+
+            vis.perPkgSimulations.forEach(sim => {
+                if (sim.sim != undefined) {
+                    sim.sim.stop();
+                }
+            })
+
+            vis.perClassSimulations.forEach(sim => {
+                if (sim.sim != undefined) {
+                    sim.sim.stop();
+                }
+            })
+
+            vis.viewLevel = "package";
+            vis.update();
+            vis.updateLinks();
+        } else if (vis.zoomLevel >= viewThresholdMethod && vis.viewLevel != "method") {
+            vis.simulation.stop();
+
+            vis.perClassSimulations.forEach(sim => {
+                if (sim.sim == undefined) {
+                    // now we need to create it, if it's the first time...
+                    sim.sim = sim.simFn();
+                    sim.sim.nodes(sim.cls).restart();
+                }
+            });
+
+            vis.perPkgSimulations.forEach(sim => {
+                if (sim.sim != undefined) {
+                    sim.sim.stop();
+                }
+            });
+
+            vis.viewLevel = "method";
+            vis.update();
+            vis.updateLinks();
         }
     }
 
@@ -295,30 +395,30 @@ class StructureVis {
         let vis = this;
 
         vis.zoomScale = d3.scaleLinear()
-        .domain([vis.minZoom, vis.maxZoom])
-        .range([0, vis.zoomBarHeight]);
+            .domain([vis.minZoom, vis.maxZoom])
+            .range([0, vis.zoomBarHeight]);
 
         vis.zoomArea = vis.chart.append("g").attr("class", "zoombar")
-        .attr("transform", `translate(${vis.width - 70}, ${vis.height - vis.zoomBarHeight - 20})`);
+            .attr("transform", `translate(${vis.width - 70}, ${vis.height - vis.zoomBarHeight - 20})`);
 
         // TODO abstract out magic constants
         vis.zoomArea.append("rect")
-        .attr("height", vis.zoomBarHeight)
-        .attr("width", 50)
-        .style("fill", "rgba(186, 85, 211, 0.5)")
-        .style("stroke", "rgb(186, 85, 211)")
-        .style("stroke-width", 2);
-        
+            .attr("height", vis.zoomBarHeight)
+            .attr("width", 50)
+            .style("fill", "rgba(186, 85, 211, 0.5)")
+            .style("stroke", "rgb(186, 85, 211)")
+            .style("stroke-width", 2);
+
 
         vis.zoomArea.selectAll("line").data(vis.transitionPoints)
-        .enter()
-        .append("line")
-        .attr("x1", 0)
-        .attr("x2", 50)
-        .attr("y1", d => vis.zoomBarHeight - vis.zoomScale(d * (vis.maxZoom - vis.minZoom)))
-        .attr("y2", d => vis.zoomBarHeight - vis.zoomScale(d * (vis.maxZoom - vis.minZoom)))
-        .style("stroke", "black")
-        .style("stroke-width", 2);
+            .enter()
+            .append("line")
+            .attr("x1", 0)
+            .attr("x2", 50)
+            .attr("y1", d => vis.zoomBarHeight - vis.zoomScale(d * (vis.maxZoom - vis.minZoom)))
+            .attr("y2", d => vis.zoomBarHeight - vis.zoomScale(d * (vis.maxZoom - vis.minZoom)))
+            .style("stroke", "black")
+            .style("stroke-width", 2);
 
         vis.updateZoomLevel();
     }
@@ -329,13 +429,13 @@ class StructureVis {
         let zoom = vis.zoomArea.selectAll("#zoom-level").data([vis.zoomLevel]);
 
         zoom.enter().append("line").attr("id", "zoom-level")
-        .merge(zoom)
-        .attr("x1", -10)
-        .attr("x2", 60)
-        .attr("y1", d => vis.zoomBarHeight - vis.zoomScale(d))
-        .attr("y2", d => vis.zoomBarHeight - vis.zoomScale(d))
-        .style("stroke", "Purple")
-        .style("stroke-width", 3);
+            .merge(zoom)
+            .attr("x1", -10)
+            .attr("x2", 60)
+            .attr("y1", d => vis.zoomBarHeight - vis.zoomScale(d))
+            .attr("y2", d => vis.zoomBarHeight - vis.zoomScale(d))
+            .style("stroke", "Purple")
+            .style("stroke-width", 3);
     }
 
     addViewButtons() {
--- vis/js/index.js
+++ vis/js/index.js
@@ -6,15 +6,22 @@ d3.json("data.json").then(data => {
 
 
     var methodLinks; // should be a similar approach...
+    // also need to associate the method and class together 
 
     // each package goes in its own group, class go in the package group
     var pkgGroups = [];
     var classGroups = [];
+    var methodGroups = [];
     data.forEach((pkg, idx) => {
         // not sre if these groups an be zero-indexed
         pkgGroups.push({id: pkg.name, group: idx + 1, type: "package", views: pkg.views || []});
         pkg.classes.forEach(cls => {
             classGroups.push({id: cls.name, group: idx + 1, pkg: pkg.name, type: "class", views: cls.views || []})
+        })
+        pkg.classes.forEach(cls => {
+            cls.methods.forEach(method => {
+                methodGroups.push({id: method.name, group: idx + 1, pkg: pkg.name, type: "method", cls: cls.name, text: method.text || "", views: cls.views || []})
+            })
         })
     })
     
@@ -28,13 +35,18 @@ d3.json("data.json").then(data => {
     {"id": "Mlle.Baptistine", "group": 1},*/
 
     var classLinks = data.map(p => p.classes.map(cls => cls.uses.map(link => {return {source: "class" + cls.name, target: "class" + link, value: 1, type: "class"}}))).flat().flat();
+
+    var methodLinks = data.map(p => p.classes.map(cls => cls.methods.map(meth => meth.uses.map(link => {return {source: "method" + meth.name, target: "method" + link, value: 1, type: "method"}})))).flat().flat().flat();
+
     console.log(classLinks);
+    console.log(methodLinks);
 
     console.log(pkgGroups);
     console.log(classGroups);
+    console.log(methodGroups);
 
     // var data = {packages: [], classes: [], packageLinks: [], classLinks: [], data: []} // original data
-    var visData = {packages: pkgGroups, classes: classGroups, packageLinks: packageLinks, classLinks: classLinks, data: data};
+    var visData = {packages: pkgGroups, classes: classGroups, packageLinks: packageLinks, classLinks: classLinks, methods: methodGroups, methodLinks: methodLinks, data: data};
 
     var vis = new StructureVis({parentElement: "#vis", data: visData})
 
