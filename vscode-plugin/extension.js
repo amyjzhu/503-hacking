458b0c3d03be0bd0e4e5d7e519dde4cfc74733ce
Thomas Schweizer <tschweiz@cs.washington.edu>
2021-02-18 21:21:57 -0800

    Add placeholder 'server' command and cleanup

--- vscode-plugin/extension.js
+++ vscode-plugin/extension.js
@@ -2,42 +2,28 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand('codemap.d3', function () {
+			const panel = vscode.window.createWebviewPanel(
+				'codemap', // Identifies the type of the webview. Used internally
+				'A D3 View', // Title of the panel displayed to the user
+				vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+				{enableScripts: true,} // Webview options
+			);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "codemap" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('codemap.d3', function () {
-		// The code you place here will be executed every time your command is executed
-
-		const panel = vscode.window.createWebviewPanel(
-			'codemap', // Identifies the type of the webview. Used internally
-			'A D3 View', // Title of the panel displayed to the user
-			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-			{enableScripts: true,} // Webview options
-		);
-
-		
-		try {
-			// And set its HTML content
 			panel.webview.html = getWebviewContent(context, panel.webview);
-		}
-		catch (e) {
-			console.log(e);
-		}
-	});
+		})
+	);
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('codemap.server', function () {
+			vscode.window.showInformationMessage('Placeholder behavior for `codemap.server`');
+		})
+	);
 }
 
 function getWebviewContent(context, webview) {
