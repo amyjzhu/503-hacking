0d88672fdf48bccaa5fdcabdcc7f4ecd84069dd6
Sahil Verma <vsahil@.cs.washington.edu>
2021-02-22 03:04:05 -0800

    Updated the parser to find the files whose methods are called by another file under the 'depends' tab

--- toy-data/refactoring-toy-example-master/src/org/xml/parser.py
+++ toy-data/refactoring-toy-example-master/src/org/xml/parser.py
@@ -1,5 +1,6 @@
-import sys, os
-import xml.etree.ElementTree as ET
+import sys, os, re
+# import xml.etree.ElementTree as ET
+from xml.etree import ElementTree as ET
 
 
 # Index file has the methods for each file, but they were missing the full signature,
@@ -26,12 +27,92 @@ def process_index_file(directories):
     # print(root, len(root), root[0].attrib, root[0].getchildren()[0].text)
 
 
+def search_file_for_functions(search_func, direct_child, file_child, directories):
+    found = False
+    # one function call
+    assert search_func.count("(") == 1
+    name, num_arguments = search_func.split("(")
+    num_arguments = num_arguments.count(", ") + 1
+    
+    for direc in directories.keys():
+        for file in directories[direc]:
+            for func in directories[direc][file]['func']:
+                # print(func, "see")
+                name_, num_arguments_ = func.split("(")
+                num_arguments_ = num_arguments_.count(", ") + 1
+                if name == name_ and num_arguments == num_arguments_:       # This does not match type of arguments. 
+                    found = True
+                    # do not update the directories, instead return
+                    # directories[direct_child][file_child]['depends'].append(file)
+                    file_it_depends_on = file
+                    break
+        
+            if found:
+                break
+    # print(direc, "Searched", found)
+        if found:
+            break
+
+    # These are native functions in Java, not called from another file
+    java_functions = ['getClass()', 'super()', 'equals()']
+    for jf in java_functions:
+        if jf in search_func:
+            return file_child
+    assert found, "search func: {}, file_child: {}".format(search_func, file_child)
+    return file_it_depends_on
+
+
+## We are findind the XML file for each java file. 
+# I think this will work for Java as each file must have a different name in Java
+def find_method_dependency(directories):
+    # print(directories)
+    all_files = os.listdir(".")
+    java_files = [i for i in all_files if ("_8java" in i) and (".xml" in i)]
+    # print(java_files)
+    assert len(java_files) == len([files for direc in directories.keys() for files in directories[direc]])
+    java_files_names = [i[:-len("_8java.xml")] for i in java_files]   # remove the last added part
+    # print(java_files_names)
+
+    for direc in directories.keys():
+        for file in directories[direc]:
+            assert file in java_files_names
+            if direc == "files_in_main_directory":
+                open_file = "../{}".format(file) + ".java"
+            else:
+                open_file = "../{}/{}".format(direc, file) + ".java"
+            with open(open_file, "r") as f:
+                content = f.readlines()
+            content = [i.strip() for i in content]
+            for line in content:
+                # match = re.search(r'[\w]+\([\w\s"]*\)', lines)
+                # Adding a semi-colon to identify function calls and leave function declarations
+                match = re.search(r'[\w]+\(.*\).*;', line)   # this gives more methods than the above one. Use this
+                if match and "println" not in match.group(0):   # this works because of short-circuting, cute. 
+                    search_func = match.group(0)
+                    if search_func.count("(") == 1:
+                        file_it_depends_on = search_file_for_functions(search_func, direc, file, directories)
+                        if file != file_it_depends_on:
+                            directories[direc][file]['depends'].add(file_it_depends_on)
+                            # directories[direc][file]['depends'].append(file_it_depends_on)
+                    # two function calls
+                    elif search_func.count("(") == 2:
+                        assert "!=" in search_func      # currently in the files
+                        function1, function2 = search_func.split(" != ")
+                        file_it_depends_on = search_file_for_functions(function1, direc, file, directories)
+                        if file != file_it_depends_on:
+                            directories[direc][file]['depends'].add(file_it_depends_on)
+                        file_it_depends_on = search_file_for_functions(function2, direc, file, directories)
+                        if file != file_it_depends_on:
+                            directories[direc][file]['depends'].add(file_it_depends_on)
+            
+    return directories
+
+
 # This method is supposed to extract the methods (with full signature) from the files.
 # This method can also extract the variables of a method
 def process_classorg_files(directories):
     files = os.listdir(".")
     classorg_files = [i for i in files if "classorg" in i]
-    # print(classorg_files)
     for file in classorg_files:
         tree = ET.parse(file)
         root = tree.getroot()
@@ -46,7 +127,8 @@ def process_classorg_files(directories):
 
         # This is for the files that are in the main directory, not inside any subdirectory. 
         if len(file_remove_org) == 1:
-            directories["files_in_main_directory"][file_remove_org[0]] = []
+            # print(file_remove_org, "SEE", directories["files_in_main_directory"])
+            directories["files_in_main_directory"][file_remove_org[0]] = {'func':[], 'depends':set()}
 
         # all the methods are in "basecoumpundref" or "sectiondef"
         for itr in root.getchildren()[0].findall("sectiondef", "basecompoundref"):
@@ -56,13 +138,13 @@ def process_classorg_files(directories):
                     method = member.find("name").text + member.find("argsstring").text
                     # This is for the files that are in the main directory, not inside any subdirectory. 
                     if len(file_remove_org) == 1:
-                        directories["files_in_main_directory"][file_remove_org[0]].append(method)
-                        # print(file_remove_org)
+                        # directories["files_in_main_directory"][file_remove_org[0]].append(method)
+                        directories["files_in_main_directory"][file_remove_org[0]]['func'].append(method)
                     elif len(file_remove_org) == 2:
-                        directories[file_remove_org[0]][file_remove_org[1]].append(method)
+                        directories[file_remove_org[0]][file_remove_org[1]]['func'].append(method)
                     else:
                         raise NotImplementedError
-    
+
     return directories
 
 
@@ -85,14 +167,17 @@ def process_dir_files():
             if "innerfile" in child.tag:
                 assert ".java" in child.text
                 x = child.text.split(".java")
-                directories[direct][x[0]] = []
+                directories[direct][x[0]] = {'func':[], 'depends':set()}    # for depends using a set
                 # print(child.text)
             # print(child.tag, child.text)
-    directories["files_in_main_directory"] = {}
-    print(directories)
-    # process_index_file(directories)
+    directories["files_in_main_directory"] = {}     # the files have not been populated yet, therefore don't replace with {func and depends}
+    # print(directories)
     directories = process_classorg_files(directories)
+    directories = find_method_dependency(directories)
     print(directories)
+    # for direc in directories:
+    #     for file in directories[direc]:
+    #         print(file, directories[direc][file]['depends'])
 
 
 if __name__ == "__main__":
