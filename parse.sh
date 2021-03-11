f560185871dbc19078e5708711c85d370bc60685
Audrey Seo <audreyleeseo@gmail.com>
2021-03-11 13:20:02 -0800

    Update parse.sh to use threshold, default data.json location
    The parse.sh script now can take the threshold option using -t, and
    the default data.json location is now vis/data/data.json, so that
    it updates the visualization's copy of the data.

--- parse.sh
+++ parse.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env bash
 
 # Requires the appropriate version of java and also maven
+DIR="$(cd $(dirname $0); pwd)"
 
 die() {
   echo "$1"
   exit 1
 }
+USAGE="Usage: parse.sh [-t #] <path/to/sources/root> [<data destination>]"
+[ "$#" -gt 0 ] || die "$USAGE"
 
-[ "$#" -gt 0 ] || die "Usage: parse.sh path/to/sources/root"
+THRESHOLD_ARG=""
+while getopts ":t:" flag; do
+  case "$flag" in
+    t) THRESHOLD_ARG="$OPTARG";;
+    \?) die "$USAGE";;
+  esac
+done
+[ $(($# - $OPTIND)) -ge 0 ] || die "$USAGE"
 
-DIR="$(cd $(dirname $0); pwd)"
+SRC_DIR="${@:$OPTIND:1}"
+DATA_DEST="${@:$OPTIND+1:1}"
+
+if [ -z "$DATA_DEST" ]; then
+  DATA_DEST="vis/data/data.json"
+fi
+
 JAR_PATH="$DIR"/codemap-java-parser/target/codemap-java-parser-1.0-SNAPSHOT-jar-with-dependencies.jar
 
 if ! [ -e "$JAR_PATH" ]; then
@@ -17,4 +33,8 @@ if ! [ -e "$JAR_PATH" ]; then
   mvn clean compile assembly:single || die "Maven failed!"
 fi
 
-java -cp "$JAR_PATH" org.codemap.parser.ResolveMethodReferences "$1"
+if [ -z "$THRESHOLD_ARG" ]; then
+  java -cp "$JAR_PATH" org.codemap.parser.ResolveMethodReferences "$SRC_DIR" "$DATA_DEST"
+else
+  java -cp "$JAR_PATH" org.codemap.parser.ResolveMethodReferences -t=${THRESHOLD_ARG} "$SRC_DIR" "$DATA_DEST"
+fi
